446c9ef91ff4e62fc56abf0373eda9ae191487fb
Miguel Suarez <msuarez@cloudbees.com>
2020-04-06 23:50:59 -0400

    adding markerfile and default values

--- vars/defineProps.groovy
+++ vars/defineProps.groovy
@@ -1,5 +1,6 @@
 def call(String file, Map defaults) {
   //use the Pipeline Utility Steps plugin readProperties step to read the jenkinsFileParams custom marker file 
+  //https://jenkins.io/doc/pipeline/steps/pipeline-utility-steps/#readproperties-read-properties-from-files-in-the-workspace-or-text
   def props = readProperties defaults: defaults, file: file
   for ( e in props ) {
     env.setProperty(e.key, e.value)
