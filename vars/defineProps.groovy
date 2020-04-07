535f31dc90549e28a7adae8692ea37d611dfb470
Miguel Suarez <msuarez@cloudbees.com>
2020-04-07 02:05:33 -0400

    add yaml

--- vars/defineProps.groovy
+++ vars/defineProps.groovy
@@ -1,13 +1,14 @@
-//def call(String file, Map defaults) {
-def call (body){
+def call(String file, Map defaults) {
+//def call (body){
   //use the Pipeline Utility Steps plugin readProperties step to read the jenkinsFileParams custom marker file 
   //https://jenkins.io/doc/pipeline/steps/pipeline-utility-steps/#readproperties-read-properties-from-files-in-the-workspace-or-text
   //def props = readProperties defaults: defaults, file: file
-  def pipelineParams= [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = pipelineParams
-  body()
-  for ( e in pipelineParams ) {
+  // def pipelineParams= [:]
+  // body.resolveStrategy = Closure.DELEGATE_FIRST
+  // body.delegate = pipelineParams
+  // body()
+  def props = readYaml(file: 'build.yaml')
+  for ( e in props ) {
     env.setProperty(e.key, e.value)
   }
 }
